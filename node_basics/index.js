204bb2aa3a96f39feac4e775f1a361aa00f23b08
Pavel Okhonko <pavel.okhonko@onereach.com>
2024-10-03 19:55:01 +0300

    chore: add start script. Add text formatting

--- node_basics/index.js
+++ node_basics/index.js
@@ -6,3 +6,4 @@ console.log(`Not formatted string: ${baseString}`);
 
 console.log(chalk.blue(`Blue color: ${baseString}`));
 
+console.log(chalk.blue.bgRed.bold(`Blue color bold text on red background: ${baseString}`))
