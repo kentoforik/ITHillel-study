f256f3fcbcb90d1a2647c9b1b654491bf9ea5e12
Pavel Okhonko <pavel.okhonko@onereach.com>
2024-10-14 20:51:00 +0300

    feat: add task4 - function args

---
+++ 6functions/task4.js
@@ -0,0 +1,27 @@
+//just additional practice with functions
+
+
+//simple ...rest
+const arrayFromNumbers = (...numbers) => numbers.map(el => el * 2);
+
+console.log('Array of numbers, mutliplied by 2: ', arrayFromNumbers(1, 2, 3))
+
+
+// ...rest + 1 arg
+const arrayFromNumbersMult = (multiplicator, ...numbers) => {
+  return numbers.map(el => el * multiplicator);
+}
+
+console.log('Array of numbers, mutliplied by provided argument: ', arrayFromNumbersMult(2, 1, 2, 3))
+
+// default arg + ...rest
+function defaultName(name = "Pavlo", ...rest) {
+  let names = [name];
+  for (let name of rest) {
+    names.push(name)
+  }
+  return names;
+}
+
+console.log('Array of names, populated with default name: ', defaultName())
+console.log('Array of names, populated with arguments: ', defaultName("Vlad", "Pavlo", "Anton"))
